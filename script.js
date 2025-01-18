20569ba19cbd6a133ab12e38015c9555ada268ff
Hariom-vishwa <hawkgamerhv@gmail.com>
2025-01-19 00:57:47 +0530

    date and time

--- script.js
+++ script.js
@@ -4,36 +4,47 @@ const inputBox = document.getElementById("input"),
 function addTask() {
   if (inputBox.value === "") {
     alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listCont.appendChild(li);
+    let delBtn = document.createElement("span");
+    delBtn.innerHTML = "&#x2716;";
+    li.appendChild(delBtn);
+    let dateTime = document.createElement("div");
+    dateTime.innerHTML = "Date and Time";
+    li.appendChild(dateTime);
+
+    // Time and Date 
+
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString();
+    const formattedTime = currentDate.toLocaleTimeString();
+    dateTime.textContent = `${formattedTime}, ${formattedDate}`;
   }
-  else{
-    let li = document.createElement("li")
-    li.innerHTML = inputBox.value
-    listCont.appendChild(li)
-    let delBtn = document.createElement("span")
-    delBtn.innerHTML = "&#x2716;"
-    li.appendChild(delBtn)
-  }
-  inputBox.value=""
-  saveData()
+  inputBox.value = "";
+  saveData();
 }
 
+listCont.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
 
-listCont.addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked");
-    saveData()
-  }
-  else if(e.target.tagName === "SPAN"){
-    e.target.parentElement.remove()
-    saveData()
-  }
-}, false);
-
-function saveData(){
-  localStorage.setItem("data", listCont.innerHTML)
+function saveData() {
+  localStorage.setItem("data", listCont.innerHTML);
 }
 
-function showTasks(){
+function showTasks() {
   listCont.innerHTML = localStorage.getItem("data");
 }
 showTasks();
